e14ae66ecc2a47760c381223c0698d0d19ff1380
wrotich <winniecherop@outlook.com>
2019-08-29 12:38:00 +0300

    fetch data with fetch api

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
 export default class App extends Component {
-  
+
   constructor() {
     super();
-  } 
+    this.state = {
+      gifs: []
+    };
+  }
 
-  render() { 
+  componentDidMount() {
+    fetch("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC")
+    .then(response => response.json())
+    .then(responseData => {
+      this.setState({ gifs: responseData.data });
+    })
+    .catch(error => {
+      console.log("Error fetching and parsing the data", error);
+    });
+  }
+
+  render() {
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
-          </div>   
-        </div>    
+            <SearchForm />
+          </div>
+        </div>
         <div className="main-content">
           <GifList />
         </div>
